6e26af72a88a940815bb41ecbf37126c5fa3d214
GeraJuarez <geracjuarez@gmail.com>
2021-09-24 09:37:22 -0700

    add unt test for controller

---
+++ src/controller/controller_test.go
@@ -0,0 +1,40 @@
+package controller_test
+
+import (
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"testing"
+
+	"github.com/gerajuarez/wize-academy-go/controller"
+	"github.com/gorilla/mux"
+)
+
+func TestMain(m *testing.M) {
+	log.SetOutput(ioutil.Discard)
+	os.Exit(m.Run())
+}
+
+func TestHello(t *testing.T) {
+	req, err := http.NewRequest("GET", "/hello", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+	handler := mux.NewRouter()
+	handler.HandleFunc("/hello", controller.Hello)
+	handler.ServeHTTP(rr, req)
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
+
+	expected := `Hello wizeline academy 2021.`
+	if rr.Body.String() != expected {
+		t.Errorf("handler returned unexpected body: got %v want %v",
+			rr.Body.String(), expected)
+	}
+}
